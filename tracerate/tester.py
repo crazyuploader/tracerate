b67e1e7453b445044e39c1b4241378a7f7b035fe
Rushil Patel <rushil13579@gmail.com>
2026-05-12 23:31:54 +0530

    [ADD] ping, download, and upload measure methods

--- tracerate/tester.py
+++ tracerate/tester.py
@@ -1,6 +1,9 @@
 import socket
 import time
 from typing import Callable
+import httpx
+import os
+
 
 SERVER = {
     "name": "Cloudflare",
@@ -8,6 +11,12 @@ SERVER = {
     "upload_url" : "https://speed.cloudflare.com/__up",
     "host": "speed.cloudflare.com",
     "port": 443,
+}
+
+REQUEST_HEADERS = {
+    "User-Agent": "Mozilla/5.0",
+    "Accept": "*/*",
+    "Referer": "https://speed.cloudflare.com/",
 }
 
 def ping(host: str, port: int, attempts: int = 5):
@@ -47,5 +56,52 @@ def ping(host: str, port: int, attempts: int = 5):
     average_latency = sum(valid_results) / len(valid_results)
     packet_loss = ((attempts - len(valid_results)) / attempts) * 100
     jitter = max(valid_results) - min(valid_results)
+    return round(average_latency, 2), round(packet_loss, 1), round(jitter, 2)
 
-    return average_latency, packet_loss, jitter
+def download(url: str, bytes: int, on_progess: Callable[[int], None] | None = None) -> float:
+    """
+    Downloads streams of data and measure speed in Mbps.
+
+    Returns: speed in Mbps, or 0.0 if it fails.
+    """
+    url = url.format(bytes=bytes)
+    try:
+        total_bytes = 0
+        start = time.perf_counter()
+        with httpx.stream("GET", url, timeout=30, follow_redirects=True, headers=REQUEST_HEADERS) as response:
+            response.raise_for_status()
+            for chunk in response.iter_bytes():
+                total_bytes += len(chunk)
+                if on_progess:
+                    on_progess(total_bytes)
+
+        end = time.perf_counter()
+        elapsed_time = (end - start)
+
+        if elapsed_time == 0 or total_bytes == 0:
+            return 0.0
+
+        speed = (total_bytes * 8) / elapsed_time / 1_000_000
+        return round(speed, 2)
+
+    except Exception:
+        return 0.0
+
+def upload(url: str, bytes: int) -> float:
+    """"
+    Uploads random bytes of data to a server and measure speed in Mbps.
+
+    Returns: upload speed in Mbps, or 0.0 if it fails.
+    """
+    data = os.urandom(bytes)
+    try:
+        start = time.perf_counter()
+        httpx.post(url, content=data)
+        end = time.perf_counter()
+        elapsed_time = end - start
+        if elapsed_time == 0:
+            return 0.0
+        speed = (bytes * 8) / elapsed_time / 1_000_000
+        return round(speed, 2)
+    except Exception:
+        return 0.0
